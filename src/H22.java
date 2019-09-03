02de8d0c5dbae9f5c44f80da7f5f19e2c9a4224d
danieljunek15 <dvandenbrink@roc-dev.com>
2019-09-03 13:57:21 +0200

    hi

---
+++ src/H22.java
@@ -0,0 +1,16 @@
+
+import java.awt.*;
+import java.applet.*;
+
+public class H22 extends Applet {
+
+    public void init() {
+    setBackground(Color.white);
+}
+public void paint(Graphics g)  {
+    g.setColor(Color.blue);
+    g.drawString("Daniël" ,180, 140);
+    g.setColor(Color.red);
+    g.drawString("van den Brink" ,160, 150);
+        }
+}
