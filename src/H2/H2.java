63457beba8a5dd1e09acf16e68fc44f1e74cd064
danieljunek15 <dvandenbrink@roc-dev.com>
2019-09-03 13:06:25 +0200

    hi

---
+++ src/H2/H2.java
@@ -0,0 +1,32 @@
+/*
+
+@(#)Show.java 1.0 03/02/26
+
+*
+
+Voorbeeld 2.1
+
+*
+
+*/
+
+//importeer de benodigde klassen uit de Java bibliotheek
+import java.awt.*;
+import java.applet.*;
+
+//een klasse met de naam Show van het type Applet
+public class H2 extends Applet {
+
+    //een (lege) methode die de Applet initialiseert
+    public void init() {
+       setBackground(Color.blue);
+
+    }
+    //een methode die de inhoud van het scherm tekent
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+
+        g.drawString("Daniël van den Brink"     , 50, 60 );
+    }
+
+}
