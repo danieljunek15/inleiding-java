760a199dc32598acd2442c2f4d3ea754a358707c
danieljunek15 <dvandenbrink@roc-dev.com>
2019-09-05 09:14:59 +0200

    hi

--- src/H4/H4o2.java
+++ src/H4/H4o2.java
@@ -9,27 +9,34 @@ public class H4o2 extends Applet {
     }
 
     public void paint(Graphics g) {
+        
+        //ik zet de achtergrond roze.
         setBackground(Color.pink);
+
+        //Ik maak de lijnen van het huis zwart.
         g.setColor(Color.black);
-     g.drawRect(80,150,200,250);
-     g.drawLine(180,35,80,150);
-     g.drawLine(280,150,180,35);
-     g.drawLine(80,270,280,270);
 
+        //Dit is de eerste en tweede etage van het huis.
+        g.drawRect(99, 319, 51, 41);
+        g.drawRect(99, 199, 51, 41);
+        g.drawRect(199, 199, 51, 41);
+        g.drawRect(80, 150, 200, 250);
+        g.drawRect(199, 299, 51, 101);
 
-     g.setColor(Color.blue);
-     g.fillRect(100,320,50,40);
+        //Hier maak ik het dak.
+        g.drawLine(180, 35, 80, 150);
+        g.drawLine(280, 150, 180, 35);
+        g.drawLine(80, 270, 280, 270);
 
+        //Dit zijn de ramen van het huis.
+        g.setColor(Color.yellow);
+        g.fillRect(100, 320, 50, 40);
+        g.fillRect(100, 200, 50, 40);
+        g.fillRect(200, 200, 50, 40);
 
-     g.fillRect(100,200,50,40);
-
-
-     g.fillRect(200,200,50,40);
-
-
-     g.setColor(Color.LIGHT_GRAY);
-     g.drawRect(200,299,50,100);
-     g.fillRect(200,299,50,100);
+        //Dit is de deur van het huis.
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(200, 300, 50, 100);
 
 
     }
