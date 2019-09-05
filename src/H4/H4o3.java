98799d0b73ef209af97f322355dfc9e417609b3d
danieljunek15 <dvandenbrink@roc-dev.com>
2019-09-05 10:36:31 +0200

    Heb de vlag gemaakt van Nederland.

--- src/H4/H4o3.java
+++ src/H4/H4o3.java
@@ -17,17 +17,20 @@ public class H4o3 extends Applet {
         //Dis is de flaggen stok.
         g.fillRect(70,95,10,300);
 
-        //Dit is de flag.
+        //Dit is de flag met kronkels er in.
+        //De roode bar.
         g.setColor(Color.red);
         g.fillRect(80,100,25,15);
         g.fillRect(130,100,25,15);
         g.fillRect(105,95,25,15);
         g.fillRect(155,95,25,15);
+        //De witte bar.
         g.setColor(Color.white);
         g.fillRect(80,115,25,15);
         g.fillRect(130,115,25,15);
         g.fillRect(105,110,25,15);
         g.fillRect(155,110,25,15);
+        //De blauwe bar.
         g.setColor(Color.blue);
         g.fillRect(80,130,25,15);
         g.fillRect(130,130,25,15);
